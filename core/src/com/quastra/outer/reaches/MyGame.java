3f69561f0475588977cc9b4f6c40b78ed5aebe13
nh100 <noahh717@gmail.com>
2015-12-25 23:37:32 -0700

    Added ashley support, and some new components related to movement and drawing.

--- core/src/com/quastra/outer/reaches/MyGame.java
+++ core/src/com/quastra/outer/reaches/MyGame.java
@@ -9,7 +9,6 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.ObjectMap;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
@@ -20,7 +19,6 @@ import java.util.Iterator;
 
 public class MyGame extends ApplicationAdapter {
 	SpriteBatch batch;
-	World world;
 	Stage currentStage;
 	ArrayMap<String, Stage> stages;
 	Engine engine;
